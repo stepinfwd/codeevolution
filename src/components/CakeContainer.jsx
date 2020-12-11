b14a8b4b7dfd4f578e56628c643e4db799f0d152
kiran <kiran@dexlock.com>
2020-12-11 13:20:11 +0530

    base redux project setup

---
+++ src/components/CakeContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CakeContainer() {
+    return (
+        <div>
+            <h2>No Of Cakes:</h2>
+            <button>BuyCake</button>
+        </div>
+    )
+}
+
+export default CakeContainer
