4750144996d8b50b395d3d85d7962f16440862bb
kiran <kiran@dexlock.com>
2020-12-15 14:52:39 +0530

    selector and dispatch hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import CakeContainer from './components/CakeContainer';
+import { Provider } from 'react-redux';
+import store from './redux/cakes/store';
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
     <CakeContainer/>
     </div>
+    </Provider>
   );
 }
 
--- src/components/CakeContainer.jsx
+++ src/components/CakeContainer.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from "react";
+import { connect } from "react-redux";
+import {buyCake} from "../redux";
 
-function CakeContainer() {
-    return (
-        <div>
-            <h2>No Of Cakes:</h2>
-            <button>BuyCake</button>
-        </div>
-    )
+function CakeContainer(props) {
+  return (
+    <div>
+      <h2>No Of Cakes:{props.noOfCakes}</h2>
+      <button onClick={props.buyCake}>BuyCake</button>
+    </div>
+  );
 }
+function mapStateToProps(state) {
+  return { noOfCakes: state.cakeNumber };
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // dispatching plain actions
+    buyCake: () => dispatch(buyCake()),
+  };
+};
 
-export default CakeContainer
+export default connect(mapStateToProps ,mapDispatchToProps)(CakeContainer);
